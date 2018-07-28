2808675324b04edbc17579f531025c0f62c2795f
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-07-28 16:18:16 +0100

    Styling and structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
+# post controller
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all.order('created_at DESC')
-	end
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
-	def new
-	end
+  def new; end
 
-	def create
-		@post = Post.new(post_params)
-		@post.save
+  def create
+    @post = Post.new(post_params)
+    @post.save
 
-		redirect_to @post
-	end
+    redirect_to @post
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-	private
+  private
 
-	def post_params
-		params.require(:post).permit(:title, :body)
-	end
-
-
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
